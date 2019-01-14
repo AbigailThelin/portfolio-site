8f00ede91315fb4fbae03c25bf7de288896ffad3
athelin <athelin@overstock.com>
2019-01-14 15:22:37 -0700

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        Home
+      </div>
+    );
+  }
+}
+
+export default Home;
+
+// kaiizen
+// tega technologies
+// overstock
+// future in design
+// girls who code
